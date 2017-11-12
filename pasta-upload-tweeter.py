5f5ba8ee9dabfa5360b5fe25d6806a5c140cf1c0
servilla <mark.servilla@gmail.com>
2017-11-11 20:34:35 -0700

    Add local DNS lookup and fix conditional logic for processing tweet

--- pasta-upload-tweeter.py
+++ pasta-upload-tweeter.py
@@ -26,9 +26,11 @@ app = Flask(__name__)
 daiquiri.setup(level=logging.INFO)
 logger = daiquiri.getLogger('pasta-upload-tweeter.py: ' + __name__)
 
-def build_pasta_url(hostname=None, package_id=None):
+
+def build_pasta_url(address=None, package_id=None):
     scope, identifier, revision = package_id.split('.')
-    pasta_url = 'https://' + hostname + '/nis/mapbrowse?scope=' + scope + \
+    client = get_domain(address=address)
+    pasta_url = 'https://' + client + '/nis/mapbrowse?scope=' + scope + \
                 '&identifier=' + identifier + '&revision=' + revision
     return pasta_url
 
@@ -38,22 +40,34 @@ def build_tweet_msg(package_id=None, pasta_url=None):
                                                               url=pasta_url)
     return msg
 
+
+def get_domain(address=None):
+    if address == '129.24.124.170':
+        return 'portal-d.lternet.edu'
+    elif address == '129.24.124.73':
+        return 'portal-s.lternet.edu'
+    elif address == '129.24.124.97':
+        return 'portal.lternet.edu'
+    return None
+
+
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
-    if request.method == 'GET':
-        return 'Testing PASTA upload tweeter!\n'
+
+    pasta_addresses = ['129.24.124.170', '129.24.124.73', '129.24.124.97']
 
     if request.method == 'POST':
         package_id = request.get_data().decode('utf-8')
-        logger.info(package_id)
-        env = request.environ
-        host = request.environ['HTTP_HOST']
-        url = build_pasta_url(hostname=host, package_id=package_id)
-        msg = build_tweet_msg(package_id=package_id, pasta_url=url)
-        status = tweet.tweet_upload(msg=msg)
-        logger.info(status)
-        return '', http.HTTPStatus.NO_CONTENT
+        remote_address = request.environ['REMOTE_ADDR']
+        if remote_address in pasta_addresses and len(package_id.split('.')) == 3:
+            url = build_pasta_url(address=remote_address, package_id=package_id)
+            msg = build_tweet_msg(package_id=package_id, pasta_url=url)
+            status = tweet.tweet_upload(msg=msg)
+            logger.info(status)
+            return '\n', http.HTTPStatus.OK
+        else:
+            return 'Bad request\n', http.HTTPStatus.BAD_REQUEST
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
