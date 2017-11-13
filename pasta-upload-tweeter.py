613049724f0ada75c18ea8dfd04fe6050cb62152
servilla <mark.servilla@gmail.com>
2017-11-12 22:38:59 -0700

    Add functions for package and pasta host names, and adding package title

--- pasta-upload-tweeter.py
+++ pasta-upload-tweeter.py
@@ -19,6 +19,7 @@ import daiquiri
 from flask import Flask
 from flask import request
 
+from eml import Eml
 import properties
 import tweet
 
@@ -36,9 +37,11 @@ def build_pasta_url(address=None, package_id=None):
     return pasta_url
 
 
-def build_tweet_msg(package_id=None, pasta_url=None):
-    msg = 'New EDI data package {pid} in PASTA: {url}'.format(pid=package_id,
-                                                              url=pasta_url)
+def build_tweet_msg(package_id=None, pasta_url=None, full_title=None):
+    title = full_title.replace('\n', '')
+    msg = 'New EDI data package {pid} ({url}): "{title}"'.format(pid=package_id,
+                                                               url=pasta_url,
+                                                               title=title)
     return msg
 
 
@@ -49,6 +52,26 @@ def get_portal_name(address=None):
         return 'portal-s.lternet.edu'
     elif address == properties.PACKAGE:
         return 'portal.lternet.edu'
+    return None
+
+
+def get_package_name(address=None):
+    if address == properties.PACKAGE_D:
+        return 'package-d.lternet.edu'
+    elif address == properties.PACKAGE_S:
+        return 'package-s.lternet.edu'
+    elif address == properties.PACKAGE:
+        return 'package.lternet.edu'
+    return None
+
+
+def get_pasta_name(address=None):
+    if address == properties.PACKAGE_D:
+        return 'pasta-d.lternet.edu'
+    elif address == properties.PACKAGE_S:
+        return 'pasta-s.lternet.edu'
+    elif address == properties.PACKAGE:
+        return 'pasta.lternet.edu'
     return None
 
 
@@ -65,8 +88,11 @@ def upload():
         package_id = request.get_data().decode('utf-8')
         remote_address = request.environ['REMOTE_ADDR']
         if remote_address in pasta_addresses and len(package_id.split('.')) == 3:
+            pasta_host = get_pasta_name(address=remote_address)
+            eml = Eml(package_id=package_id, pasta_host=pasta_host)
+            eml_title = eml.title
             url = build_pasta_url(address=remote_address, package_id=package_id)
-            msg = build_tweet_msg(package_id=package_id, pasta_url=url)
+            msg = build_tweet_msg(package_id=package_id, pasta_url=url, full_title=eml_title)
             status = tweet.tweet_upload(msg=msg)
             logger.info(status)
             return '\n', http.HTTPStatus.OK
@@ -75,4 +101,5 @@ def upload():
 
 
 if __name__ == '__main__':
+    # Host must be set to 0.0.0.0 for use other than localhost, and no debug
     app.run(host='0.0.0.0')
