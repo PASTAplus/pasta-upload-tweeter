2092f6ff9496b17c89ed4ee408acb82cae051163
servilla <mark.servilla@gmail.com>
2017-12-14 21:12:54 -0700

    Post update(s) to allow continuation of long message

--- tweet.py
+++ tweet.py
@@ -12,15 +12,9 @@
     11/9/17
 """
 
-import logging
-
-import daiquiri
 import twitter
 
 import properties
-
-daiquiri.setup(level=logging.INFO)
-logger = daiquiri.getLogger('tweet.py: ' + __name__)
 
 
 def tweet_upload(msg=None):
@@ -29,21 +23,11 @@ def tweet_upload(msg=None):
     access_key = properties.ACCESS_KEY
     access_secret = properties.ACCESS_SECRET
 
-    if msg is None:
-        msg = 'Testing PASTA update tweeter!'
-
-    status = None
-
-    try:
-        api = twitter.Api(consumer_key=consumer_key,
-                          consumer_secret=consumer_secret,
-                          access_token_key=access_key,
-                          access_token_secret=access_secret)
-        status = api.PostDirectMessage(text=msg, screen_name='GauchoSays')
-    except Exception as e:
-        logger.error(e)
-
-    return status
+    api = twitter.Api(consumer_key=consumer_key,
+                      consumer_secret=consumer_secret,
+                      access_token_key=access_key,
+                      access_token_secret=access_secret)
+    api.PostUpdates(status=msg, continuation='\u2026')
 
 
 def main():
