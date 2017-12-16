8f0c28acaa1b327965ce974f9e48a4fa230f711a
servilla <mark.servilla@gmail.com>
2017-12-15 22:02:15 -0700

    Truncation msg to 280 with ellipsis and add back logging support

--- pasta_upload_tweeter.py
+++ pasta_upload_tweeter.py
@@ -13,7 +13,10 @@
 """
 
 import http
+import logging
+import os
 
+import daiquiri
 from flask import Flask
 from flask import request
 
@@ -21,7 +24,13 @@ from eml import Eml
 import properties
 import tweet
 
+cwd = os.path.dirname(os.path.realpath(__file__))
+logfile = cwd + '/pasta_upload_tweeter.log'
+daiquiri.setup(level=logging.INFO, outputs=(daiquiri.output.File(logfile),))
+logger = daiquiri.getLogger(__name__)
+
 app = Flask(__name__)
+
 
 def build_pasta_url(address=None, package_id=None):
     scope, identifier, revision = package_id.split('.')
@@ -36,6 +45,10 @@ def build_tweet_msg(package_id=None, pasta_url=None, full_title=None):
     msg = 'New EDI data package {pid} ({url}): "{title}"'.format(pid=package_id,
                                                                url=pasta_url,
                                                                title=title)
+    if len(msg) > 280:
+        trunc_msg = msg[0:279]
+        msg = trunc_msg + '\u2026'
+
     return msg
 
 
@@ -82,23 +95,32 @@ def upload():
             remote_address = properties.PACKAGE_D
         else:
             msg = 'Request not from a PASTA server!\n'
+            logger.error('Unknown address: {addr}'.format(addr=remote_address))
             return msg, http.HTTPStatus.BAD_REQUEST
 
     package_id = request.get_data().decode('utf-8')
+    logger.info(
+        'Remote address and pid: {addr}, {pid}'.format(addr=remote_address,
+                                                       pid=package_id))
     if len(package_id.split('.')) == 3:
         pasta_host = get_pasta_name(address=remote_address)
         try:
             eml = Eml(package_id=package_id, pasta_host=pasta_host)
             eml_title = eml.title
             url = build_pasta_url(address=remote_address, package_id=package_id)
-            msg = build_tweet_msg(package_id=package_id, pasta_url=url, full_title=eml_title)
-            tweet.tweet_upload(msg=msg)
+            msg = build_tweet_msg(package_id=package_id, pasta_url=url,
+                                  full_title=eml_title)
+            logger.info('Tweet message: {msg}'.format(msg=msg))
+            status = tweet.tweet_upload(msg=msg)
+            logger.info('{status}'.format(status=status))
             return '\n', http.HTTPStatus.OK
         except Exception as e:
             msg = str(e) + '\n'
+            logger.error('Unknown error: {e}'.format(e=msg))
             return msg, http.HTTPStatus.INTERNAL_SERVER_ERROR
     else:
-        msg = 'Request package identifier is not parsable into scope.identifier.revision!\n'
+        msg = 'Request package identifier not parsable!\n'
+        logger.error('Unknown request body: {pid}'.format(pid=package_id))
         return msg, http.HTTPStatus.BAD_REQUEST
 
 
