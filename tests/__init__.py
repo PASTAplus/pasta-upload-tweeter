56496a523ef4b3da4c868da62bdece6e8567f295
Mark Servilla <mark.servilla@gmail.com>
2017-12-30 20:25:11 -0700

    Add test suite for EML

---
+++ tests/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: __init__
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    12/30/17
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_eml.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: test_eml
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    12/30/17
+"""
+
+import unittest
+import logging
+
+from eml import Eml
+
+logger = logging.getLogger('test_eml')
+
+
+class TestEml(unittest.TestCase):
+    eml = None
+
+    @classmethod
+    def setUpClass(cls):
+        with open('./knb-lter-nin.1.1.xml', 'r') as f:
+            xml = f.read().encode()
+        TestEml.eml = Eml(eml=xml)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_get_title(self):
+        title = 'Daily Water Sample Nutrient Data for North Inlet Estuary, ' \
+                'South Carolina, from 1978 to 1992, North Inlet LTER'
+        eml_title = TestEml.eml.title
+        self.assertEqual(title, eml_title)
+
+
+if __name__ == '__main__':
+    unittest.main()
