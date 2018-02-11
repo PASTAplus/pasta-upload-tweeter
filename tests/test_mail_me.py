7cb5dfa7034f4f804048a67bfdcbc6085004ce70
servilla <mark.servilla@gmail.com>
2018-02-11 15:47:58 -0700

    Add daiquiri to class setup for local logging

--- tests/test_mail_me.py
+++ tests/test_mail_me.py
@@ -13,14 +13,24 @@
 """
 
 import logging
+import os
 import unittest
+
+import daiquiri
 
 import mail_me as mm
 import properties
-logger = logging.getLogger('test_mail_me')
 
 
 class TestMailMe(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        logfile = cwd + '/pasta_upload_tweeter.log'
+        daiquiri.setup(level=logging.INFO,
+                       outputs=(daiquiri.output.File(logfile),))
+        logger = daiquiri.getLogger(__name__)
 
     def setUp(self):
         pass
