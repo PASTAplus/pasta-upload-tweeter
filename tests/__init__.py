a6c63264809a8419e395433020de1be228942b39
servilla <mark.servilla@gmail.com>
2018-02-12 05:32:02 -0700

    Add logging setup for tests (only works cleanly with pytest CLI now)

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,10 +12,17 @@
     12/30/17
 """
 
+import logging
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+import daiquiri
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+logfile = cwd + '/tests.log'
+daiquiri.setup(level=logging.INFO,
+               outputs=(daiquiri.output.File(logfile),))
 
 def main():
     return 0
