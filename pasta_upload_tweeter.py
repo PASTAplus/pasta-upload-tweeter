2f2c088e8c3a8681644ab4033a42121a500476d2
servilla <mark.servilla@gmail.com>
2018-02-13 20:44:28 -0700

    Refactor mail_me signature

--- pasta_upload_tweeter.py
+++ pasta_upload_tweeter.py
@@ -89,13 +89,14 @@ def upload():
             logger.info('Tweet message: {msg}'.format(msg=msg))
             status = tweet.tweet_upload(msg=msg)
             logger.info('{status}'.format(status=status))
-            mm.mail_me(status='INFO', msg=str(status).encode(),
+            mm.mail_me(subject='INFO: '+__name__, msg=str(status),
                        to=properties.MAIL_TO)
             return '\n', http.HTTPStatus.OK
         except Exception as e:
             msg = str(e) + '\n'
             logger.error('Unknown error: {e}'.format(e=msg))
-            mm.mail_me(status='ERROR', msg=msg.encode(), to=properties.MAIL_TO)
+            mm.mail_me(subject='ERROR: '+__name__, msg=msg,
+                       to=properties.MAIL_TO)
             return msg, http.HTTPStatus.INTERNAL_SERVER_ERROR
     else:
         msg = 'Request package identifier not recognized\n'
