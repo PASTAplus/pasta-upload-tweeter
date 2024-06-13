7449a99a7ba569a427d9594795e0ceb93510d763
Mark Servilla <mark.servilla@gmail.com>
2024-06-13 16:44:49 -0600

    Fix deletion of wrong line

--- pasta_upload_tweeter.py
+++ pasta_upload_tweeter.py
@@ -82,7 +82,7 @@ def upload():
         try:
             eml = Eml(get_eml(package_id=package_id))
             url = build_pasta_url(package_id=package_id)
-            msg += f"\nTwitter and Elon Musk suck!"
+            msg = build_tweet_msg(package_id=package_id, pasta_url=url, title=eml.title)
             subject = '(INFO) ' + __name__ + ' ' + package_id
             mimemail.send_mail(subject=subject, msg=msg)
             return '\n', http.HTTPStatus.OK
